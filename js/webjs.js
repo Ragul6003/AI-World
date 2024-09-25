7825747ae5b5618109167e30530ac2d715a9ba3c
RAGUL S R <122031906+Ragul6003@users.noreply.github.com>
2024-09-25 21:52:38 +0530

    Add files via upload

---
+++ js/webjs.js
@@ -0,0 +1,296 @@
+
+let web1 = document.getElementById("web1");
+let w1 = document.getElementById("web1-content");
+let web2 = document.getElementById("web2");
+let w2 = document.getElementById("web2-content");
+let web3 = document.getElementById("web3");
+let w3 = document.getElementById("web3-content");
+let web4 = document.getElementById("web4");
+let w4 = document.getElementById("web4-content");
+let web5 = document.getElementById("web5");
+let w5 = document.getElementById("web5-content");
+let web6 = document.getElementById("web6");
+let w6 = document.getElementById("web6-content");
+let web7 = document.getElementById("web7");
+let w7 = document.getElementById("web7-content");
+let web8 = document.getElementById("web8");
+let w8 = document.getElementById("web8-content");
+let web9 = document.getElementById("web9");
+let w9 = document.getElementById("web9-content");
+let web10 = document.getElementById("web10");
+let w10 = document.getElementById("web10-content");
+
+let title = document.getElementById('name');
+
+
+w1.style.display = "none";
+w2.style.display = "none";
+w3.style.display = "none";
+w4.style.display = "none";
+w5.style.display = "none";
+w6.style.display = "none";
+w7.style.display = "none";
+w8.style.display = "none";
+w9.style.display = "none";
+w10.style.display = "none";
+
+function showweb1(name) {
+    title.innerText = name;
+    web1.style.display = "none";
+    w1.style.display = "block";
+    web2.style.display = "none";
+    w2.style.display = "none";
+    web3.style.display = "none";
+    w3.style.display = "none";
+    web4.style.display = "none";
+    w4.style.display = "none";
+    web5.style.display = "none";
+    w5.style.display = "none";
+    web6.style.display = "none";
+    w6.style.display = "none";
+    web7.style.display = "none";
+    w7.style.display = "none";
+    web8.style.display = "none";
+    w8.style.display = "none";
+    web9.style.display = "none";
+    w9.style.display = "none";
+    web10.style.display = "none";
+    w10.style.display = "none";
+
+}
+
+function showweb2(name) {
+    title.innerText = name;
+    web1.style.display = "none";
+    w1.style.display = "none";
+    web2.style.display = "none";
+    w2.style.display = "block";
+    web3.style.display = "none";
+    w3.style.display = "none";
+    web4.style.display = "none";
+    w4.style.display = "none";
+    web5.style.display = "none";
+    w5.style.display = "none";
+    web6.style.display = "none";
+    w6.style.display = "none";
+    web7.style.display = "none";
+    w7.style.display = "none";
+    web8.style.display = "none";
+    w8.style.display = "none";
+    web9.style.display = "none";
+    w9.style.display = "none";
+    web10.style.display = "none";
+    w10.style.display = "none";
+
+}
+
+function showweb3(name) {
+    title.innerText = name;
+    web1.style.display = "none";
+    w1.style.display = "none";
+    web2.style.display = "none";
+    w2.style.display = "none";
+    web3.style.display = "none";
+    w3.style.display = "block";
+    web4.style.display = "none";
+    w4.style.display = "none";
+    web5.style.display = "none";
+    w5.style.display = "none";
+    web6.style.display = "none";
+    w6.style.display = "none";
+    web7.style.display = "none";
+    w7.style.display = "none";
+    web8.style.display = "none";
+    w8.style.display = "none";
+    web9.style.display = "none";
+    w9.style.display = "none";
+    web10.style.display = "none";
+    w10.style.display = "none";
+
+}
+
+function showweb4(name) {
+    title.innerText = name;
+    web1.style.display = "none";
+    w1.style.display = "none";
+    web2.style.display = "none";
+    w2.style.display = "none";
+    web3.style.display = "none";
+    w3.style.display = "none";
+    web4.style.display = "none";
+    w4.style.display = "block";
+    web5.style.display = "none";
+    w5.style.display = "none";
+    web6.style.display = "none";
+    w6.style.display = "none";
+    web7.style.display = "none";
+    w7.style.display = "none";
+    web8.style.display = "none";
+    w8.style.display = "none";
+    web9.style.display = "none";
+    w9.style.display = "none";
+    web10.style.display = "none";
+    w10.style.display = "none";
+
+}
+
+function showweb5(name) {
+    title.innerText = name;
+    web1.style.display = "none";
+    w1.style.display = "none";
+    web2.style.display = "none";
+    w2.style.display = "none";
+    web3.style.display = "none";
+    w3.style.display = "none";
+    web4.style.display = "none";
+    w4.style.display = "none";
+    web5.style.display = "none";
+    w5.style.display = "block";
+    web6.style.display = "none";
+    w6.style.display = "none";
+    web7.style.display = "none";
+    w7.style.display = "none";
+    web8.style.display = "none";
+    w8.style.display = "none";
+    web9.style.display = "none";
+    w9.style.display = "none";
+    web10.style.display = "none";
+    w10.style.display = "none";
+
+}
+
+function showweb6(name) {
+    title.innerText = name;
+    web1.style.display = "none";
+    w1.style.display = "none";
+    web2.style.display = "none";
+    w2.style.display = "none";
+    web3.style.display = "none";
+    w3.style.display = "none";
+    web4.style.display = "none";
+    w4.style.display = "none";
+    web5.style.display = "none";
+    w5.style.display = "none";
+    web6.style.display = "none";
+    w6.style.display = "block";
+    web7.style.display = "none";
+    w7.style.display = "none";
+    web8.style.display = "none";
+    w8.style.display = "none";
+    web9.style.display = "none";
+    w9.style.display = "none";
+    web10.style.display = "none";
+    w10.style.display = "none";
+
+}
+function showweb7(name) {
+    title.innerText = name;
+    web1.style.display = "none";
+    w1.style.display = "none";
+    web2.style.display = "none";
+    w2.style.display = "none";
+    web3.style.display = "none";
+    w3.style.display = "none";
+    web4.style.display = "none";
+    w4.style.display = "none";
+    web5.style.display = "none";
+    w5.style.display = "none";
+    web6.style.display = "none";
+    w6.style.display = "none";
+    web7.style.display = "none";
+    w7.style.display = "block";
+    web8.style.display = "none";
+    w8.style.display = "none";
+    web9.style.display = "none";
+    w9.style.display = "none";
+    web10.style.display = "none";
+    w10.style.display = "none";
+
+}
+function showweb8(name) {
+    title.innerText = name;
+    web1.style.display = "none";
+    w1.style.display = "none";
+    web2.style.display = "none";
+    w2.style.display = "none";
+    web3.style.display = "none";
+    w3.style.display = "none";
+    web4.style.display = "none";
+    w4.style.display = "none";
+    web5.style.display = "none";
+    w5.style.display = "none";
+    web6.style.display = "none";
+    w6.style.display = "none";
+    web7.style.display = "none";
+    w7.style.display = "none";
+    web8.style.display = "none";
+    w8.style.display = "block";
+    web9.style.display = "none";
+    w9.style.display = "none";
+    web10.style.display = "none";
+    w10.style.display = "none";
+
+}
+function showweb8(name) {
+    title.innerText = name;
+    web1.style.display = "none";
+    w1.style.display = "none";
+    web2.style.display = "none";
+    w2.style.display = "none";
+    web3.style.display = "none";
+    w3.style.display = "none";
+    web4.style.display = "none";
+    w4.style.display = "none";
+    web5.style.display = "none";
+    w5.style.display = "none";
+    web6.style.display = "none";
+    w6.style.display = "none";
+    web7.style.display = "none";
+    w7.style.display = "none";
+    web8.style.display = "none";
+    w8.style.display = "none";
+    web9.style.display = "none";
+    w9.style.display = "block";
+    web10.style.display = "none";
+    w10.style.display = "none";
+
+}
+function showweb10(name) {
+    title.innerText = name;
+    web1.style.display = "none";
+    w1.style.display = "none";
+    web2.style.display = "none";
+    w2.style.display = "none";
+    web3.style.display = "none";
+    w3.style.display = "none";
+    web4.style.display = "none";
+    w4.style.display = "none";
+    web5.style.display = "none";
+    w5.style.display = "none";
+    web6.style.display = "none";
+    w6.style.display = "none";
+    web7.style.display = "none";
+    w7.style.display = "none";
+    web8.style.display = "none";
+    w8.style.display = "none";
+    web9.style.display = "none";
+    w9.style.display = "none";
+    web10.style.display = "none";
+    w10.style.display = "block";
+
+}
+// function validate() {
+//     let email = document.getElementById("mail").value;
+//     let password = document.getElementById("pass").value;
+
+//     if (document.getElementById("check").checked) {
+//         if ((email == "sriramakrishnan005@gmail.com" && password == "Sriram@005") || (email == "srihari6112003@gmail.com" && password == "Haridevil#7810")) {
+//             window.open('/admin/admin.html');
+//         }
+//         else
+//             alert("Enter correct Details");
+//     }
+//     else {
+//         window.open('index.html');
+//     }
+// }
